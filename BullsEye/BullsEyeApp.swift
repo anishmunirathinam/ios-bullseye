57b1151798227865bbd8fcbd6cf77fe9032dc6b0
Anish <anish.munirathinam@gmail.com>
2022-12-06 13:24:09 +0530

    🎉 Inital commit

---
+++ BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Munirathinam, Anish | RIEPL on 06/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ios-bullseye
+🎯 BullsEye is an interactive and fun game to play. Where you need to accurately match the target score shown.
